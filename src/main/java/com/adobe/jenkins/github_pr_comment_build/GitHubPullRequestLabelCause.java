11fc1d9eec91cf38877573ba9ebcd67200f4ff31
agtiwari <agtiwari@adobe.com>
2023-04-06 12:28:46 +0530

    Add label trigger support

---
+++ src/main/java/com/adobe/jenkins/github_pr_comment_build/GitHubPullRequestLabelCause.java
@@ -0,0 +1,68 @@
+package com.adobe.jenkins.github_pr_comment_build;
+
+import hudson.model.Cause;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.Whitelisted;
+import org.kohsuke.stapler.export.Exported;
+
+import java.io.Serializable;
+
+/**
+ * Created by Agyaey on 04/04/2023.
+ */
+public final class GitHubPullRequestLabelCause extends Cause implements Serializable {
+    private final String labelUrl;
+    private final String labellingAuthor;
+    private final String label;
+
+    /**
+     * Constructor.
+     *
+     * @param labelUrl        the URL for the GitHub Label
+     * @param labellingAuthor the author of the GitHub Label
+     * @param label           the body for the GitHub Label
+     */
+    public GitHubPullRequestLabelCause(String labelUrl, String labellingAuthor, String label) {
+        this.labelUrl = labelUrl;
+        this.labellingAuthor = labellingAuthor;
+        this.label = label;
+    }
+
+    @Whitelisted
+    @Override
+    public String getShortDescription() {
+        return "GitHub pull request Label";
+    }
+
+    /**
+     * Retrieves the URL for the GitHub Label for this cause.
+     *
+     * @return the URL for the GitHub Label
+     */
+    @Whitelisted
+    @Exported(visibility = 3)
+    public String getLabelUrl() {
+        return labelUrl;
+    }
+
+    /**
+     * Retrieves the author of the GitHub Label for this cause.
+     *
+     * @return the author of the GitHub Label
+     */
+    @Whitelisted
+    @Exported(visibility = 3)
+    public String getLabellingAuthor() {
+        return labellingAuthor;
+    }
+
+    /**
+     * Retrieves the body for the GitHub Label for this cause.
+     *
+     * @return the body for the GitHub Label
+     */
+    @Whitelisted
+    @Exported(visibility = 3)
+    public String getLabel() {
+        return label;
+    }
+}
---
+++ src/main/java/com/adobe/jenkins/github_pr_comment_build/TriggerPRLabelBranchProperty.java
@@ -0,0 +1,53 @@
+package com.adobe.jenkins.github_pr_comment_build;
+
+import hudson.Extension;
+import hudson.model.Job;
+import hudson.model.Run;
+import jenkins.branch.BranchProperty;
+import jenkins.branch.BranchPropertyDescriptor;
+import jenkins.branch.JobDecorator;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * Allows a GitHub pull request comment to trigger an immediate build based on a comment string.
+ */
+public class TriggerPRLabelBranchProperty extends BranchProperty {
+    /**
+     * The comment body to trigger a new build on.
+     */
+    private final String label;
+
+    /**
+     * Constructor.
+     *
+     * @param label the comment body to trigger a new build on
+     */
+    @DataBoundConstructor
+    public TriggerPRLabelBranchProperty(String label) {
+        this.label = label;
+    }
+
+    /**
+     * The comment body to trigger a new build on.
+     *
+     * @return the comment body to use
+     */
+    public String getLabel() {
+        return this.label;
+    }
+
+    @Override
+    public <P extends Job<P, B>, B extends Run<P, B>> JobDecorator<P, B> jobDecorator(Class<P> clazz) {
+        return null;
+    }
+
+    @Extension
+    public static class DescriptorImpl extends BranchPropertyDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return Messages.TriggerPRLabelBranchProperty_trigger_on_pull_request_label();
+        }
+
+    }
+}
